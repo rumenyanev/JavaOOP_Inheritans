c0effac0d8aa30b909b1c6351a14b111e1f00880
rumenyanev <zaispanez@gmail.com>
2019-07-09 11:00:47 +0300

    .

---
+++ src/StackOfStrings/Main.java
@@ -0,0 +1,19 @@
+package StackOfStrings;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+    }
+}
+/*⦁	Stack of Strings
+Create a class Stack which can store only strings and has the following functionality:
+⦁	Private field: data: ArrayList<String>
+⦁	Public method: push(String item): void
+⦁	Public method: pop(): String
+⦁	Public method: peek(): String
+⦁	Public method: isEmpty(): boolean
+
+Hints
+Use composition/delegation in order to have a field in which to store the stack's data*/
